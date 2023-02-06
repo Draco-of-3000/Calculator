0e5c4cf6375d5def6e794612eb346f6e6dacfebb
Draco <kevin.tenisson@gmail.com>
2023-02-06 07:39:56 -0800

    added subtraction function

--- calculator.js
+++ calculator.js
@@ -1,7 +1,17 @@
-function addition() {
+function addition(){
     let sum = 0;
     for (let i = 0; i < arguments.length; i++){
         sum += arguments[i];
     }
     return sum;
 }
+
+function subtraction(){
+    let difference = [0];
+    for(let i = 0; i < arguments.length; i--){
+        minus -= arguemnts[i];
+    }
+    return difference;
+}
+
+
