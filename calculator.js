3b0ab702f60a071198f5cb759f78a28781692395
Draco <kevin.tenisson@gmail.com>
2023-02-09 07:41:47 -0800

    Fixed NaN issue

--- calculator.js
+++ calculator.js
@@ -32,21 +32,21 @@ function division(){
 
 const operatorButtons = document.querySelectorAll('.operators .operator');
 
-function operate(operator, num1, num2){
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
-
-    switch (operator) {
-        case '+':
-            return addition(num1, num2);
-        case '-':
-            return subtraction(num1, num2);
-        case '*':
-            return multiplication(num1, num2);
-        case '/':
-            return division(num1, num2);
-        default:
-            return "Select an operator";
+function operate(operation, num1, num2){
+    if (operation === '+'){
+        return addition(num1, num2);
+    }
+    else if (operation === '-'){
+        return subtraction(num1, num2);
+    }
+    else if (operation === '*'){
+        return multiplication(num1, num2);
+    }
+    else if (operation === '/'){
+        return division(num1, num2);
+    }
+    else {
+        return "Your code failed nigga"
     }
 }
 
@@ -76,61 +76,51 @@ document.addEventListener('DOMContentLoaded', function populateDisplay(){
 
 
 
-let firstNumber;
-let secondNumber;
-let operation;
+let firstNumber = null;
 let operatorClicked = false;
-let numbers = document.querySelectorAll('.number')
 let equal = document.querySelector('.equals');
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
+    let numbers = document.querySelectorAll('.number');
+    let operation = "";
+    let secondNumber = "";
     numbers.forEach(number => {
         number.addEventListener('click', () => {
-            if(!operatorClicked){
-                let tempFirstNumber = parseInt(number.textContent);
-                firstNumber = tempFirstNumber;
-                displayValue = firstNumber;
-                console.log(typeof(firstNumber));
+            let textContent = number.textContent;
+            if (!isNaN(textContent)) {
+                if (!operatorClicked) {
+                    firstNumber = firstNumber ? firstNumber * 10 + Number(textContent) : Number(textContent);
+                } else {
+                    secondNumber = secondNumber ? secondNumber * 10 + Number(textContent) : Number(textContent);
+                }
+                let displayValue = `${firstNumber} ${operation || ""} ${secondNumber || ""}`;
+                document.querySelector(".display1").textContent = displayValue;
+            } else {
+                console.error("textContent is not a number:", textContent);
             }
-            else {
-                secondNumber = parseInt(number.textContent);
-                displayValue = `${firstNumber} ${operation} ${secondNumber}`;
-                console.log(typeof(secondNumber));
-            };
-            document.querySelector(".display1").textContent = displayValue;
         });
-    });      
-})
+    });
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
     let operators = document.querySelectorAll('.operator');
     operators.forEach(operator => {
         operator.addEventListener('click', () => {
-            console.log("Operator clicked: " + operator.textContent);
             operatorClicked = true;
             operation = operator.textContent;
-            firstNumber = parseInt(displayValue);
-            displayValue = `${firstNumber} ${operation}` + " ";
+            let displayValue = `${firstNumber} ${operation}${secondNumber ? "" : " "}`;
             document.querySelector(".display1").textContent = displayValue;
         });
     });
-})
 
-
-
-document.addEventListener("DOMContentLoaded", function() {
     let equals = document.querySelector(".equals");
     equals.addEventListener("click", function() {
-        let num1 = parseInt(firstNumber);
-        let num2 = parseInt(secondNumber);
-        console.log("typeof num1:", typeof num1);
-        console.log("value of num1:", num1);
-        console.log("typeof num2:", typeof num2);
-        console.log("value of num2:", num2);
+        let num1 = Number(firstNumber);
+        let num2 = Number(secondNumber);
         result = operate(operation, num1, num2);
         document.querySelector('.display2').textContent = result;
-        console.log(result);
+        firstNumber = result;
+        secondNumber = null;
+        operation = "";
+        operatorClicked = false;
     });
 });
+
